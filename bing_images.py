2a793aab91f57f33f01d7b7fcbc10572386ec7bc
Allencs <allenchensheng@163.com>
2020-01-17 09:34:06 +0800

    use asyncio.gather

--- bing_images.py
+++ bing_images.py
@@ -41,20 +41,21 @@ class BingImages(object):
                 self.client.get_link(url, self.management))
             self.tasks.append(t)
 
-        for t in self.tasks:
-            await t
+        await asyncio.gather(*self.tasks)
 
         logger.info('下载链接获取完成，开始下载')
         self.tasks.clear()
 
     async def main(self):
 
+        # t = asyncio.create_task(self.client.download(self.management.downloadLinks))
+        # self.tasks.append(t)
+
         for downloadLink in self.management.downloadLinks:
             t = asyncio.create_task(self.client.download(downloadLink))
             self.tasks.append(t)
 
-        for task in self.tasks:
-            await task
+        await asyncio.gather(*self.tasks)
 
         duration = time.time() - self.startTime
         logger.info("用时： %.2fs" % duration)
